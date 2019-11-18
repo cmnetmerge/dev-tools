6665b8d245b7159abe2b006eaa48f420f848b995
chenmeng <chenmeng@100.me>
2019-11-18 13:44:21 +0800

    first commit

---
+++ src/main/java/cn/moc/lang/StringUtils.java
@@ -0,0 +1,13 @@
+package cn.moc.lang;
+
+/**
+ * @author : chenmeng
+ * @version 1.0.0
+ * @date : Created in 18-11-2019  11:57
+ * @description :
+ **/
+public class StringUtils {
+
+    private StringUtils() {
+    }
+}
